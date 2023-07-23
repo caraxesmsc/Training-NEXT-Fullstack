d3754aa21e05100b1be4e125ff25a59c7cc94022
caraxesmsc <mohamedongo21@gmail.com>
2023-07-23 19:21:19 +0300

    Lecture4 custom function handson auth

--- Lecture4/action.js
+++ Lecture4/action.js
@@ -6,3 +6,17 @@ function sayHello(name){
 function myProduct (x,y){
     return x*y;
 }
+function authbool(user,pass){
+    let presetUser="Ahmed";
+    let presetEmail="Ahmed@gmail.com"; 
+    let presetPass=123;
+    if((user==presetUser || user==presetEmail) && pass==presetPass){
+        return true;
+    }else{
+        return false;
+    }
+}
+function auth(bool){
+    let msg = bool ? "Access Granted" : "DENIED";
+    return msg;
+}
