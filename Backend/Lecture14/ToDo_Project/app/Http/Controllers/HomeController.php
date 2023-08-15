8fc0fb063b4eb025641b17b9f4dfd83b76596e58
caraxesmsc <mohamedongo21@gmail.com>
2023-08-15 22:02:37 +0300

    todo app

--- Backend/Lecture14/ToDo_Project/app/Http/Controllers/HomeController.php
+++ Backend/Lecture14/ToDo_Project/app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\todoModel;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +23,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tasks=todoModel::all();
+        return view('home')->with('data',$tasks);
+    }
+    public function new()
+    {
+        return view('new');
+    }
+    public function add(request $request)
+    {
+        todoModel::create($request -> all());
+        return redirect(to:'/home');
     }
 }
--- Backend/Lecture14/ToDo_Project/resources/views/home.blade.php
+++ Backend/Lecture14/ToDo_Project/resources/views/home.blade.php
@@ -14,7 +14,39 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are loggedD in!') }}
+                    <div>
+                        <a class="btn btn-primary" href="/new">New Task</a>
+                    </div>
+
+
+                </div>
+            </div>
+            <div class="card bg-primary text-light">
+                <div class="card-header">Tasks</div>
+                <div class="card-body">
+                <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Task</th>
+                    <th scope="col">Details</th>
+                    <th scope="col">Deadline</th>
+                    <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $value)
+                    <tr>
+                    <td>{{$value->id}}</td>
+                    <td>{{$value->name}}</td>
+                    <td>{{$value->details}}</td>
+                    <td>{{$value->deadline}}</td>
+                    <td><a href="" class="btn btn-outline-danger">Done</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                </table>
                 </div>
             </div>
         </div>
